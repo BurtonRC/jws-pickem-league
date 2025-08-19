a86b7a0ed551b3377808468fb397b9e78877c3d5
unknown <[200~143559469+BurtonRC@users.noreply.github.com~>
2025-08-19 15:36:31 +0100

    Update site

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './index.css'; // Tailwind/custom CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
