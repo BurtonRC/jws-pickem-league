167d48df26c6c1b7dc76943320fc31d447ed30a4
unknown <[200~143559469+BurtonRC@users.noreply.github.com~>
2025-08-20 02:46:06 +0100

    Update site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  base: mode === "production" ? "/jws-pickem-league/" : "/",
-}));
+  base: process.env.NODE_ENV === "production" ? "/jws-pickem-league/" : "/",
+});
