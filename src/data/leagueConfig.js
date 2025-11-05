f114d5608cb4e04a23a871642ceed171267aabb9
unknown <[200~143559469+BurtonRC@users.noreply.github.com~>
2025-11-05 22:13:19 +0000

    Update Week 9 v2

--- src/data/leagueConfig.js
+++ src/data/leagueConfig.js
@@ -1,32 +1,19 @@
 // data/leagueConfig.js
-export const driveByGames = {
-  
-"401772943": "Miami Dolphins", // Game ID → DB team
+export const driveByGames = { 
 
-"401772871": "Minnesota Vikings",
-
-"401772872": "Carolina Panthers",
-
-"401772764": "Tennessee Titans",
-
-"401772763": "Atlanta Falcons",
-
-"401772874": "New Orleans Saints"
+"401772944": "Las Vegas Raiders", // Game ID → DB team
+"401772636": "Atlanta Falcons",
+"401772771": "Miami Dolphins",
+"401772877": "New Orleans Saints",
+"401772878": "Washington Commanders",
+"401772773": "Arizona Cardinals"
 
   // add all other DB games for the week
 };
 
 export const pointSpreads = {
-  "401772926": [-3.5]  
+  "401772927": [3.5]  
   // "Green Bay -2.5 (Home)" and "Detroit +2.5 (Away)" in your dropdown automatically
   // Home gets minus ------ Away gets plus when entering positive no.
   // To reverse it put in a negative no.
 };
-
-
-
-
-
-
-
-
