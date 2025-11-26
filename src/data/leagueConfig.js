4efbc551e183d9edb9c4207dee69039eba8ab036
unknown <[200~143559469+BurtonRC@users.noreply.github.com~>
2025-11-26 23:29:52 +0000

    Update Week 13 v1

--- src/data/leagueConfig.js
+++ src/data/leagueConfig.js
@@ -1,20 +1,31 @@
 // data/leagueConfig.js
 export const driveByGames = { 
 
-"401772946": "Houston Texans",
-"401772781": "Cincinnati Bengals",
-"401772888": "New York Giants",
-"401772887": "Minnesota Vikings",
-"401772886": "Tennessee Titans",
-"401772782": "New York Jets",
-"401772929": "Tampa Bay Buccaneers",
-"401772820": "Carolina Panthers"
+"401772930": "Cincinnati Bengals",
+
+"401772621": "Chicago Bears",
+
+"401772785": "Cleveland Browns",
+
+"401772786": "Tennessee Titans",
+
+"401772892": "New Orleans Saints",
+
+"401772894": "Carolina Panthers",
+
+"401772896": "Minnesota Vikings",
+
+"401772788": "Las Vegas Raiders",
+
+"401772931": "Washington Commanders",
+
+"401772821": "New York Giants"
 
   // add all other DB games for the week
 };
 
 export const pointSpreads = {
-  "401772929": [6.5]  
+  "401772694": [3.5]  
   // "Green Bay -2.5 (Home)" and "Detroit +2.5 (Away)" in your dropdown automatically
   // Home gets minus ------ Away gets plus when entering positive no.
   // To reverse it put in a negative no.
