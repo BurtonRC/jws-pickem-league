93c195ece7e80529f9491ac7495d5695436e2d19
unknown <[200~143559469+BurtonRC@users.noreply.github.com~>
2026-09-13 18:01:39 +0100

    Add diagnostic to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const originalPushState = window.history.pushState;
+window.history.pushState = function (...args) {
+  console.trace("[NAVIGATION] pushState:", args);
+  return originalPushState.apply(this, args);
+};
+
+const originalReplaceState = window.history.replaceState;
+window.history.replaceState = function (...args) {
+  console.trace("[NAVIGATION] replaceState:", args);
+  return originalReplaceState.apply(this, args);
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
